ad23b758b3b83c7ca6b34c53592983a5dfba60cc
akbarrdev <teamnevolution@gmail.com>
2025-02-12 04:07:36 +0700

    Improve image splitting grid generation and background positioning

--- canvas/script.js
+++ canvas/script.js
@@ -3,20 +3,23 @@ const y = document.getElementById("y");
 const container = document.querySelector(".container");
 
 function split() {
-  const posX = x.value,
-    posY = y.value;
+  const posX = parseInt(x.value),
+    posY = parseInt(y.value);
 
   container.innerHTML = "";
   container.style.gridTemplateRows = `repeat(${posX}, 1fr)`;
   container.style.gridTemplateColumns = `repeat(${posY}, 1fr)`;
 
-  for (let i = 0; i < posY; i++) {
-    for (let a = 0; a < posX; a++) {
-      container.innerHTML += `<div class="item" style="background-position: ${
-        100 / posX
-      }% ${100 / posY}%; background-size: ${(100 / posX - 1) * a}% ${
-        (100 / posY - 1) * i
-      }%;"></div>`;
+  for (let i = 0; i < posX; i++) {
+    for (let j = 0; j < posY; j++) {
+      container.innerHTML += `
+          <div class="item" style="
+            background-position: ${(j * 100) / (posY - 1)}% ${
+        (i * 100) / (posX - 1)
+      }%;
+            background-size: ${posY * 100}% ${posX * 100}%;
+          "></div>
+        `;
     }
   }
 }
